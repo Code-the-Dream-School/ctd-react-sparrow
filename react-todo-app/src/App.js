f2c74cb4cc47458e3e8a8134399bc47650332582
aokounev <andre.okounev@gmail.com>
2022-03-31 09:30:01 +0300

    Lesson 1.1

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,22 +1,37 @@
 import * as React from "react";
 
-const welcome = {
-  greeting: "Hey",
-  title: "React",
-};
+const todoList = [
+  {
+    title: "Complete assignment",
+    id: 1,
+  },
+  {
+    title: "Read React book",
+    id: 2,
+  },
+  {
+    title: "Watch React videos",
+    id: 3,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>Todo List</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <label htmlFor="add">Add Task: </label>
+      <input id="add" type="text" />
+
+      <hr />
+
+      <ul>
+        {todoList.map(function (item) {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 
 export default App;
-
