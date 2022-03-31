23286791b59a8525cdf5d7fa749ae30fe19c1cd4
aokounev <andre.okounev@gmail.com>
2022-03-31 03:42:21 +0300

    first commit

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function App() {
+  return (
+    <div>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;
+
