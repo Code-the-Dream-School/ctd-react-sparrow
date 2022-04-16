ea44f03711cfbd9087344c7a92dd3f4cbcd06abd
Jayzar <jayzar.lynn@gmail.com>
2022-04-16 15:31:55 -0400

    Started lesson 1.5, added custom hook to start storing todolist in local storage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'; 
+import { useEffect } from 'react/cjs/react.production.min';
 import AddToDoForm from './addToDoForm';
 import ToDoList from './todolist';
 
 
-
 function App() {
-  const [toDoList, setToDoList]= React.useState([]);
   const addToDo= (newToDo)=> {
     setToDoList([...toDoList, newToDo])
   }
+
+  const useSemiPersistentState = () => {
+      const [toDoList, setToDoList]= React.useState(JSON.parse(localStorage.getItem('savedToDoList')));
+
+       useEffect(() => {
+        localStorage.setItem('savedToDoList', JSON.stringify(toDoList))}, [toDoList]);
+      return [toDoList, setToDoList];
+  }
+ 
   return (
     <div style={{ textAlign: 'center' }}>
      <h1>To Do List</h1>
