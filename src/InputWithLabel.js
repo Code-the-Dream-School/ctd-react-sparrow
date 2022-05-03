d133675051d83ac9ffdbd235e092c9f32a8ea166
Jayzar <jayzar.lynn@gmail.com>
2022-05-03 12:17:47 -0400

    Starting Lesson 1.7

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,13 +1,13 @@
 import React, { useRef, useEffect } from 'react';
 
-const InputWithLabel = (props) => {
+const InputWithLabel = ({children, toDoTitle, handleTitleChange}) => {
     const inputRef = React.useRef();
   useEffect(() =>
         {inputRef.current.focus()}, []);
     return (
         <>
-            <label htmlFor="toDoTitle">{props.children}</label>
-            <input id="toDoTitle" type="text" name="title" value={props.toDoTitle} onChange={props.handleTitleChange} ref={inputRef}/>
+            <label htmlFor="toDoTitle">{children}</label>
+            <input id="toDoTitle" type="text" name="title" value={toDoTitle} onChange={handleTitleChange} ref={inputRef}/>
         </>
     ) 
 }
--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 function ToDoListItem({todo, onRemoveToDo}) {
+   //unsure if I need to keep in id={todo.id} in li? works without it
  return (
     <>
-      <li>
+      <li id={todo.id}>
       {todo.title}
       <button onClick={() => onRemoveToDo(todo.id)}>Remove</button>
       </li>
-      
+      {console.log(todo)}
     </>
  )
 };
