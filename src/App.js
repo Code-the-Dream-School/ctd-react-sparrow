f11c1015df75d39ffa148e80e3b4f0e6dba7a014
Summer Becker <becker.summer@outlook.com>
2022-05-28 22:09:31 -0400

    lesson 1-9

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 import TodoListItem from './TodoListItem';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [todoList, setTodoList] = React.useState(
@@ -41,15 +42,27 @@ function App() {
     setTodoList(newTodoList);
   };
   return (
-    <>
-      <header>
-        
-        <h1>Todo List</h1>
-        </header>
-        <AddTodoForm onAddTodo={addTodo}/>
-        {isLoading ? (<p>Loading...</p>) : (
-        <TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}       
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={
+            <>
+          <header>
+              <h1>Todo List</h1>
+            </header>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading ? (<p>Loading...</p>) : (
+              <TodoList todoList={todoList} onRemoveTodo={removeTodo} />)}
+              </>
+          }/>
+          <Route path="/new" element={
+            <header>
+              <h1>New Todo List</h1>
+            </header>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
