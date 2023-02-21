5512be93c0ade3ed2f66d9985da7996c8de12aee
aokounev <andre.okounev@gmail.com>
2023-02-21 08:46:55 +0300

    CSS Modules

--- react-todo-app/src/Components/TodoListItem/TodoListItem.js
+++ react-todo-app/src/Components/TodoListItem/TodoListItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import style from "./TodoListItem.module.css";
 
 function TodoListItem({ todo, onRemoveTodo }) {
   return (
-    <li className="li" id={todo.id}>
+    <li className={style.listItem} id={todo.id}>
       {todo.fields.Title}
 
       <button
-        className="rm"
+        className={style.button}
         onClick={() => {
           onRemoveTodo(todo.id);
         }}
